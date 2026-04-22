ea067e6ab6fb6a65f0cfd6da458ea50f36c6a650
JeffMboya <jangina.mboya@gmail.com>
2026-04-22 12:57:01 +0300

    Align style with supermq/magistrala conventions
    - Use api.MaxLimitSize from pkg/api instead of local duplicate constant
    - Add missing doc comments to GetPropletAliveHistory mock methods
    - Add missing doc comments to GetAliveHistory proplet repository mock
    - Fix Expecter parameter types from any to interface{}
    - Fix run() call formatting to multi-line style for 4-arg mocks

--- manager/api/requests.go
+++ manager/api/requests.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	apiutil "github.com/absmach/magistrala/api/http/util"
+	"github.com/absmach/propeller/pkg/api"
 	"github.com/absmach/propeller/pkg/cron"
 	pkgerrors "github.com/absmach/propeller/pkg/errors"
 	"github.com/absmach/propeller/pkg/proplet"
@@ -15,10 +16,7 @@ import (
 
 var errStatusFilterUnsupported = errors.New("status filter is not supported")
 
-const (
-	maxMetadataBytes = 1048576 // 1MB
-	maxLimitSize     = 100
-)
+const maxMetadataBytes = 1048576 // 1MB
 
 type taskReq struct {
 	task.Task `json:",inline"`
@@ -164,7 +162,7 @@ type listTasksReq struct {
 }
 
 func (r *listTasksReq) validate() error {
-	if r.limit > maxLimitSize || r.limit < 1 {
+	if r.limit > api.MaxLimitSize || r.limit < 1 {
 		return apiutil.ErrLimitSize
 	}
 
